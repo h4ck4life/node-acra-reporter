dd9a25a73031b533682fc898d2d244c48e52eec9
Gavin <gavin@kodekoan.com>
2013-01-18 17:10:50 -0800

    node files don't need to wrap in functio() to use 'use strict'

--- routes/api.js
+++ routes/api.js
@@ -1,39 +1,37 @@
-(function() {
-    "use strict";
-    var hashFields = {
-        'SETTINGS_SECURE':1, // contains sub hash too
-        'SETTINGS_SYSTEM':1,
-        'CRASH_CONFIGURATION':1,
-        'BUILD':1,
-        'DISPLAY':1,
-        'INITIAL_CONFIGURATION':1,
-        'ENVIRONMENT':1
-    };
+"use strict";
+var hashFields = {
+    'SETTINGS_SECURE':1, // contains sub hash too
+    'SETTINGS_SYSTEM':1,
+    'CRASH_CONFIGURATION':1,
+    'BUILD':1,
+    'DISPLAY':1,
+    'INITIAL_CONFIGURATION':1,
+    'ENVIRONMENT':1
+};
 
-    var mongo = require('mongodb');
-    var states = require('../lib/states');
-    var myutils = require('../lib/utils');
+var mongo = require('mongodb');
+var states = require('../lib/states');
+var myutils = require('../lib/utils');
 
-    var index = function(req,res) {
-        res.send("Nothing here");
-    };
-    var submit_acra_report = function(req, res) {
-        var report = myutils.strObjectToData(req.body);
-        report.report_time = new Date();
-        report.state = states.STATE_NEW;
+var index = function(req,res) {
+    res.send("Nothing here");
+};
+var submit_acra_report = function(req, res) {
+    var report = myutils.strObjectToData(req.body);
+    report.report_time = new Date();
+    report.state = states.STATE_NEW;
 
-        req.mongo.collection('reports').insert(report, {w:1}, function(err, result) {
-            if (err) {
-                console.error("Error inserting into mongo:", err, result);
-                res.send("FAIL",500);
-                return;
-            }
-            res.send("OK");
-        });
-    };
+    req.mongo.collection('reports').insert(report, {w:1}, function(err, result) {
+        if (err) {
+            console.error("Error inserting into mongo:", err, result);
+            res.send("FAIL",500);
+            return;
+        }
+        res.send("OK");
+    });
+};
 
-    exports.setupRoutes = function(app) {
-        app.get('/api/', app.basicAuth, index);
-        app.post('/api/submit_acra_report', app.basicAuth, submit_acra_report);
-    };
-})();
+exports.setupRoutes = function(app) {
+    app.get('/api/', app.basicAuth, index);
+    app.post('/api/submit_acra_report', app.basicAuth, submit_acra_report);
+};
--- web.js
+++ web.js
@@ -1,105 +1,102 @@
-(function() {
-    "use strict";
+"use strict";
 
-    /* Nodefly has to be the first item loaded */
-    if (process.env.NODEFLY_KEY)
-    {
-        require('nodefly').profile(
-                process.env.NODEFLY_KEY,
-                ['acra-reporting']
-        );
-    }
-    var express = require('express');
-    var express_resource = require('express-resource');
-    var util    = require('util');
-    var url     = require("url");
-    var fs      = require('fs');
-    var MongoClient = require('mongodb').MongoClient;
+/* Nodefly has to be the first item loaded */
+if (process.env.NODEFLY_KEY)
+{
+    require('nodefly').profile(
+            process.env.NODEFLY_KEY,
+            ['acra-reporting']
+    );
+}
+var express = require('express');
+var express_resource = require('express-resource');
+var util    = require('util');
+var url     = require("url");
+var fs      = require('fs');
+var MongoClient = require('mongodb').MongoClient;
 
-    /* Config */
+/* Config */
 
-    var config = {
-        users: {},
-        mongodb: process.env.MONGO_URL || 'mongodb://localhost/node-acra-reporting'
-    };
+var config = {
+    users: {},
+    mongodb: process.env.MONGO_URL || 'mongodb://localhost/node-acra-reporting'
+};
 
-    var routes = {};
-    fs.readdir(__dirname + '/routes/', function(err, files) {
-        files.forEach(function(file) {
-            if (file.substr(-3) === '.js') {
-                console.log("Now loading route file: ", file.trim());
-                var basename = file.substr(0,file.length-3);
-                routes[basename] = require('./routes/'+basename);
-            }
-        });
-    });
-
-    if (process.env.REPORTING_CREDENTIALS)
-    {
-        process.env.REPORTING_CREDENTIALS.split(',').forEach(function(a) {
-            var sp = a.split(':');
-            config.users[sp[0]] = sp[1];
-        });
-    }
-
-    /* Callback after everything else is connected  to start express */
-    var startExpress = function(db) {
-
-        var app = express();
-
-        /* Basic Auth Function */
-        app.basicAuth = express.basicAuth(function(username, password) {
-            if (!config.users[username]) return false;
-            return config.users[username] === password;
-        }, 'Restrict area, please identify');
-
-        app.configure(function() {
-            app.engine('ejs', require('ejs-locals'));
-            app.set('view engine', 'ejs');
-            app.set('view options', { 'layout':'layout.ejs'} );
-            app.use(express.logger());
-            app.use(express['static'](__dirname + '/public'));
-            app.use(express.bodyParser());
-            app.use(express.cookieParser());
-            app.use(express.session({ 
-                secret: process.env.SESSION_SECRET || 'DemtAimfild9' 
-            }));
-            // http://www.senchalabs.org/connect/favicon.html
-            app.use(express.favicon());
-            app.use(function(req,res,next) {
-                req.mongo = db;
-                next();
-            });
-            app.use(app.router);
-
-        });
-
-        app.configure('development', function(){
-            app.use(express.errorHandler());
-        });
-
-        // listen to the PORT given to us in the environment
-        var port = process.env.PORT || 3000;
-        app.listen(port, function() {
-            console.log("Listening on " + port);
-        });
-
-        app.locals.title = "ACRA Reporter";
-
-        // http://stackoverflow.com/questions/9213707/express-resources-with-authentication-middleware
-        Object.keys(routes).forEach(function(r) {
-            routes[r].setupRoutes(app);
-        });
-
-    };
-
-    MongoClient.connect(config.mongodb, function(err, db) {
-        if (err) {
-            console.error('connection error:', err);
-        } else {
-            console.log("Connected to ", config.mongodb);
-            startExpress(db);
+var routes = {};
+fs.readdir(__dirname + '/routes/', function(err, files) {
+    files.forEach(function(file) {
+        if (file.substr(-3) === '.js') {
+            console.log("Now loading route file: ", file.trim());
+            var basename = file.substr(0,file.length-3);
+            routes[basename] = require('./routes/'+basename);
         }
     });
+});
 
-})();
+if (process.env.REPORTING_CREDENTIALS)
+{
+    process.env.REPORTING_CREDENTIALS.split(',').forEach(function(a) {
+        var sp = a.split(':');
+        config.users[sp[0]] = sp[1];
+    });
+}
+
+/* Callback after everything else is connected  to start express */
+var startExpress = function(db) {
+
+    var app = express();
+
+    /* Basic Auth Function */
+    app.basicAuth = express.basicAuth(function(username, password) {
+        if (!config.users[username]) return false;
+        return config.users[username] === password;
+    }, 'Restrict area, please identify');
+
+    app.configure(function() {
+        app.engine('ejs', require('ejs-locals'));
+        app.set('view engine', 'ejs');
+        app.set('view options', { 'layout':'layout.ejs'} );
+        app.use(express.logger());
+        app.use(express['static'](__dirname + '/public'));
+        app.use(express.bodyParser());
+        app.use(express.cookieParser());
+        app.use(express.session({ 
+            secret: process.env.SESSION_SECRET || 'DemtAimfild9' 
+        }));
+        // http://www.senchalabs.org/connect/favicon.html
+        app.use(express.favicon());
+        app.use(function(req,res,next) {
+            req.mongo = db;
+            next();
+        });
+        app.use(app.router);
+
+    });
+
+    app.configure('development', function(){
+        app.use(express.errorHandler());
+    });
+
+    // listen to the PORT given to us in the environment
+    var port = process.env.PORT || 3000;
+    app.listen(port, function() {
+        console.log("Listening on " + port);
+    });
+
+    app.locals.title = "ACRA Reporter";
+
+    // http://stackoverflow.com/questions/9213707/express-resources-with-authentication-middleware
+    Object.keys(routes).forEach(function(r) {
+        routes[r].setupRoutes(app);
+    });
+
+};
+
+MongoClient.connect(config.mongodb, function(err, db) {
+    if (err) {
+        console.error('connection error:', err);
+    } else {
+        console.log("Connected to ", config.mongodb);
+        startExpress(db);
+    }
+});
